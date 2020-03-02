3cbd2c60ff191f25601d263d2c96b0243fa2cbda
so-suke <attoutekimasshu@gmail.com>
2020-03-03 08:53:31 +0900

    Updates

---
+++ precache-manifest.3448cd6c57c9818a487204aab0a6c58f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea15ea42218684b9f3922a50f936a52a",
+    "url": "/pati-jikyu/index.html"
+  },
+  {
+    "revision": "0cb2b9eb3e68abc2b6f4",
+    "url": "/pati-jikyu/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9254db3eef16ef33a718",
+    "url": "/pati-jikyu/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0cb2b9eb3e68abc2b6f4",
+    "url": "/pati-jikyu/static/js/2.f91d772a.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/pati-jikyu/static/js/2.f91d772a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9254db3eef16ef33a718",
+    "url": "/pati-jikyu/static/js/main.bad38fae.chunk.js"
+  },
+  {
+    "revision": "1c10e3c9c78ba394ce20",
+    "url": "/pati-jikyu/static/js/runtime-main.ceedfa6d.js"
+  }
+]);
